6ba5c4406efd84a460d6c229a57a20895b66fcda
nikita-skobov <nikita@skobov.io>
2023-03-05 15:20:42 -0600

    updates example

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -2,11 +2,18 @@ use hira::{close, create_lambda, set_build_bucket, set_deploy_region, set_stack_
 
 set_build_bucket!("put-the-name-of-your-s3-bucket-here");
 set_deploy_region!("us-east-1");
-set_stack_name!("hello-world-stack");
+set_stack_name!("hello-world-stack4");
 
 #[create_lambda]
-fn hello_world(event: String) -> String {
-    format!("Your event was {event}")
+async fn hello_world(_event: String) -> String {
+    // this invokes the 'apples' lambda function
+    let apples_str = apples(2).await;
+    format!("You have {apples_str}")
+}
+
+#[create_lambda]
+async fn apples(n: usize) -> String {
+    format!("{n} apples")
 }
 
 close!();
