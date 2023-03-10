541778c8880c04549c844e9aa05843f2eb263567
nikita-skobov <nikita@skobov.io>
2023-03-10 17:33:38 -0600

    add example for using env vars

---
+++ examples/using_environment_variables/src/main.rs
@@ -0,0 +1,38 @@
+use hira::{close, create_lambda, set_build_bucket, set_deploy_region, set_stack_name, secret_from_dot_env, const_from_dot_env_or_default, load_dot_env};
+use serde_json::Value;
+
+// this is optional. by default we assume your environment
+// variables are in ".env". If you need to specify a different path you
+// can call load_dot_env!() before anything else
+load_dot_env!("MY_ENV_FILE.txt");
+
+// load a variable from a .env file, or use default if not found:
+const_from_dot_env_or_default!(BUILD_BUCKET, "put-your-bucket-here");
+
+// load a variable from a .env file, but it is only available at compile time.
+secret_from_dot_env!(SECRET_STACK_NAME);
+
+// can either provide a const, or a string literal:
+set_build_bucket!(BUILD_BUCKET);
+set_deploy_region!("us-east-1");
+set_stack_name!(SECRET_STACK_NAME);
+
+
+#[create_lambda({
+    triggers: [{ "type": "function_url" }],
+    policy_statements: [{
+        "action": "lambda:InvokeFunction",
+        "resource": "arn:aws:lambda:*:*:function:apples"
+    }],
+})]
+async fn hello_world(_event: Value) -> String {
+    // this is valid because BUILD_BUCKET is a publically accessible constant
+    println!("{}", BUILD_BUCKET);
+    // This is NOT valid because we declared SECRET_STACK_NAME a secret,
+    // and therefore it does not exist at runtime:
+    // println!("{}", SECRET_STACK_NAME);
+
+    format!("hello world")
+}
+
+close!();
