8f1ccf848a87897480981c05c70bcaafad6ce60d
nikita-skobov <nikita@skobov.io>
2023-03-18 14:25:30 -0600

    update static website example to use modules

--- examples/static_website_ez/src/main.rs
+++ examples/static_website_ez/src/main.rs
@@ -2,23 +2,36 @@ use hira::{
     close,
     set_deploy_region,
     set_stack_name,
-    create_static_website,
     const_from_dot_env_or_default
 };
 
-// to deploy this you'll either need to create a .env file with these values
-// or modify the default values in this file to be a domain you
-// own and certificate for that domain.
-const_from_dot_env_or_default!(WEBSITE_URL, "your.website-here.com");
+// to deploy this you'll either need to create a .env file with ACM_ARN=aws:arn...
+// or modify the default values in this file to be your cert ARN
 const_from_dot_env_or_default!(ACM_ARN, "aws:arn:...your-acm-arn-here");
-
 set_deploy_region!("us-east-1");
-set_stack_name!("hello-world-stack5");
+set_stack_name!("example-static-website-ez");
 
-#[create_static_website({
-    url: WEBSITE_URL,
-    acm_arn: ACM_ARN,
+
+#[hira::module("hira:aws_s3", {
+    bucket_name: "hira-example-static-website-ez",
+    public_website: {},
 })]
-pub mod my_website {}
+mod mybucket {
+    // This gets called during deploy time to initialize your S3 bucket!
+    pub async fn _init() {
+        let client = self::make_s3_client().await;
+        let html = "<html><body><h1>hello world</h1></body></html>";
+        self::put_object_builder(&client, "index.html", html.into())
+            .content_type("text/html")
+            .send().await.expect("Failed to initialize S3 bucket");
+    }
+}
+
+#[hira::module("hira:aws_r53_cdn", {
+    acm_cert_arn: ACM_ARN,
+})]
+const MYSITE: () = match ("yourwebsite.com", "path") {
+    ("yourwebsite.com", "*") => (mybucket::WEBSITE_URL, "*", "originProtocol=http-only"),
+};
 
 close!();
