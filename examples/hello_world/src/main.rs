5cb463c09e090605e083e220eaa657089f1e02e4
nikita-skobov <nikita@skobov.io>
2023-03-18 13:59:24 -0600

    update hello world example to use module registry

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,4 +1,4 @@
-use hira::{close, create_lambda, set_build_bucket, set_deploy_region, set_stack_name};
+use hira::{close, set_build_bucket, set_deploy_region, set_stack_name};
 use serde_json::Value;
 
 hira::const_from_dot_env!(BUILD_BUCKET);
@@ -6,23 +6,23 @@ set_build_bucket!(BUILD_BUCKET);
 set_deploy_region!("us-east-1");
 set_stack_name!("example-hello-world");
 
-#[create_lambda({
+#[hira::module("hira:aws_lambda", {
     triggers: [{ "type": "function_url" }],
     policy_statements: [{
         "action": "lambda:InvokeFunction",
         "resource": "arn:aws:lambda:*:*:function:apples"
     }],
 })]
-async fn hello_world(event: Value) -> String {
+async fn hello_world(event: Value) -> Result<String> {
     println!("{:#?}", event);
     // this invokes the 'apples' lambda function
-    let apples_str = apples(2).await;
-    format!("You have {apples_str}")
+    let apples_str = apples(2usize).await?;
+    Ok(format!("You have {apples_str}"))
 }
 
-#[create_lambda]
-async fn apples(n: usize) -> String {
-    format!("{n} apples")
+#[hira::module("hira:aws_lambda", {})]
+async fn apples(n: usize) -> Result<String> {
+    Ok(format!("{n} apples"))
 }
 
 close!();
