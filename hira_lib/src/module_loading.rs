6aa6fb7313c5f346872e910fc78099fa214ff001
nikita-skobov <nikita@skobov.io>
2023-04-26 18:26:34 -0600

    adds module file loading/parsing

--- hira_lib/src/module_loading.rs
+++ hira_lib/src/module_loading.rs
@@ -1,11 +1,11 @@
+use std::collections::HashMap;
+
 use proc_macro2::TokenStream;
 use quote::{quote, format_ident, ToTokens};
-use syn::{parse_file, File};
+use syn::parse_file;
 
-use crate::HiraConfig;
-use crate::parsing::{default_stream, compiler_error};
-
-use super::parsing::get_list_of_strings;
+use super::HiraConfig;
+use super::parsing::{default_stream, compiler_error, iterate_file, iterate_expr_for_strings, get_list_of_strings};
 use super::use_hira_config;
 
 #[derive(Debug)]
@@ -24,6 +24,12 @@ pub struct HiraModule {
     pub name: String,
     pub loaded_from: LoadedFrom,
     pub contents: String,
+
+    pub required_hira_modules: Vec<String>, // syn::Item::Const
+    pub required_crates: Vec<String>, // syn::Item::Const
+    pub export_items: HashMap<String, String>, // anything pub
+    pub primary_export_item: String, // syn::Item::Struct
+    pub entrypoint_fn: Option<String>, // syn::Item::Fn
 }
 
 impl Default for HiraModule {
@@ -32,6 +38,11 @@ impl Default for HiraModule {
             name: Default::default(),
             loaded_from: LoadedFrom::Implied,
             contents: Default::default(),
+            required_crates: Default::default(),
+            export_items: Default::default(),
+            primary_export_item: Default::default(),
+            required_hira_modules: Default::default(),
+            entrypoint_fn: Default::default(),
         }
     }
 }
@@ -50,20 +61,189 @@ impl HiraModule {
     }
 }
 
-fn load_module_external_file(conf: &mut HiraConfig, path: String) -> Result<HiraModule, String> {
-    let mut out = load_module_implied_file(conf, path)?;
-    out.loaded_from = LoadedFrom::ExternalFile;
-    Ok(out)
+fn set_entrypoint_fn(module: &mut HiraModule, item: &syn::ItemFn) -> bool {
+    if item.sig.ident.to_string() != "wasm_entrypoint" {
+        return true
+    }
+    // enforce 2 args: the first is the LibraryObj
+    // the 2nd is the callback to the user's function.
+    // but too lazy to parse the callback signature right now. we just assume its valid..
+    let input = if item.sig.inputs.len() != 2 {
+        return true
+    } else {
+        item.sig.inputs.first().unwrap()
+    };
+    let input = match input {
+        syn::FnArg::Typed(t) => t,
+        _ => return true,
+    };
+    let reference = match *input.ty {
+        syn::Type::Reference(ref r) => r.clone(),
+        _ => return true,
+    };
+    if reference.mutability.is_none() {
+        return true
+    }
+    let type_path = match *reference.elem {
+        syn::Type::Path(p) => p,
+        _ => return true,
+    };
+    let first = match type_path.path.segments.first() {
+        Some(s) => s,
+        None => return true,
+    };
+    if first.ident.to_string() != "LibraryObj" {
+        return true
+    }
+    // we verified this is the wasm_entrypoint fn, so set its signature to the module
+    module.entrypoint_fn = Some(item.sig.to_token_stream().to_string());
+    true
+}
+
+fn set_required_crates(module: &mut HiraModule, item: &syn::ItemConst) -> bool {
+    if item.ident.to_string() != "REQUIRED_CRATES" {
+        return true;
+    }
+    iterate_expr_for_strings(&*item.expr, |a| {
+        module.required_crates.push(a);
+    });
+    true
+}
+
+fn set_required_hira_mods(module: &mut HiraModule, item: &syn::ItemConst) -> bool {
+    if item.ident.to_string() != "REQUIRED_HIRA_MODULES" {
+        return true;
+    }
+    iterate_expr_for_strings(&*item.expr, |a| {
+        module.required_hira_modules.push(a);
+    });
+    true
+}
+
+fn set_primary_export_item(module: &mut HiraModule, item: &syn::ItemType) -> bool {
+    if item.ident.to_string() != "ExportType" {
+        return true;
+    }
+
+    if let syn::Type::Path(ref p) = *item.ty {
+        if let Some(seg) = p.path.segments.last() {
+            if p.path.segments.len() == 1 {
+                module.primary_export_item = seg.ident.to_string();
+            }
+        }
+    }
+    true
+}
+
+/// to add typehints while writing hira modules, writers
+/// can add a #[hira::hira] macro above some const item.
+/// we must remove these from the wasm code otherwise it'll try
+/// to load some macro that doesn't exist at compile time for wasm modules.
+fn remove_recursive_hira_macro(_module: &mut HiraModule, item: &syn::ItemMod) -> bool {
+    for item in &item.attrs {
+        let path = item.meta.path();
+        let path_str = path.to_token_stream().to_string();
+        if path_str.contains("hira") {
+            return false;
+        }
+    }
+    true
+}
+
+/// made this into a module because then i can collapse it while editing.
+/// all of these are almost the same
+mod set_exports {
+    use super::*;
+
+    pub fn set_export_item_enum(module: &mut HiraModule, item: &syn::ItemEnum) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
+    pub fn set_export_item_mod(module: &mut HiraModule, item: &syn::ItemMod) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
+    pub fn set_export_item_static(module: &mut HiraModule, item: &syn::ItemStatic) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
+    pub fn set_export_item_struct(module: &mut HiraModule, item: &syn::ItemStruct) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
+    pub fn set_export_item_trait(module: &mut HiraModule, item: &syn::ItemTrait) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
+    pub fn set_export_item_union(module: &mut HiraModule, item: &syn::ItemUnion) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
+    pub fn set_export_item_fn(module: &mut HiraModule, item: &syn::ItemFn) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.sig.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
+    pub fn set_export_item_const(module: &mut HiraModule, item: &syn::ItemConst) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
+    pub fn set_export_item_type(module: &mut HiraModule, item: &syn::ItemType) -> bool {
+        if let syn::Visibility::Public(_) = item.vis {
+            module.export_items.insert(item.ident.to_string(), item.to_token_stream().to_string());
+        }
+        true
+    }
 }
 
 /// Note: this function doesn't know where the module was loaded from. it sets loaded_from to Implied
 /// by default, but the caller of this function should override this value.
-fn load_module_from_file_string(conf: &mut HiraConfig, module_file: File) -> Result<HiraModule, String> {
-    Ok(HiraModule {
-        name: "todo".to_string(),
-        contents: module_file.to_token_stream().to_string(),
-        loaded_from: LoadedFrom::Implied,
-    })
+fn load_module_from_file_string(_conf: &mut HiraConfig, path: &str, module_string: String) -> Result<HiraModule, String> {
+    let mut out = HiraModule::default();
+    
+    let module_file = match parse_file(&module_string) {
+        Ok(p) => p,
+        Err(e) => {
+            return Err(format!("Failed to parse '{}' as valid rust code.\nError:\n{:?}", path, e));
+        }
+    };
+
+    let contents = iterate_file(
+        &mut out, module_file,
+        &[set_entrypoint_fn, set_exports::set_export_item_fn],
+        &[set_required_crates, set_required_hira_mods, set_exports::set_export_item_const],
+        &[set_primary_export_item, set_exports::set_export_item_type],
+        &[set_exports::set_export_item_enum],
+        &[set_exports::set_export_item_mod, remove_recursive_hira_macro],
+        &[set_exports::set_export_item_static],
+        &[set_exports::set_export_item_struct],
+        &[set_exports::set_export_item_trait],
+        &[set_exports::set_export_item_union],
+    );
+    out.contents = contents;
+    Ok(out)
+}
+
+
+fn load_module_external_file(conf: &mut HiraConfig, path: String) -> Result<HiraModule, String> {
+    let mut out = load_module_implied_file(conf, path)?;
+    out.loaded_from = LoadedFrom::ExternalFile;
+    Ok(out)
 }
 
 /// Path must be absolute prior to calling this. Even though it's implied. this just makes
@@ -75,13 +255,8 @@ fn load_module_implied_file(conf: &mut HiraConfig, path: String) -> Result<HiraM
             return Err(format!("Failed to read hira module from path '{}'\nError:\n{:?}", path, e));
         }
     };
-    let parsed_wasm_code = match parse_file(&module_file) {
-        Ok(p) => p,
-        Err(e) => {
-            return Err(format!("Failed to parse '{}' as valid rust code.\nError:\n{:?}", path, e));
-        }
-    };
-    let mut out = load_module_from_file_string(conf, parsed_wasm_code)?;
+
+    let mut out = load_module_from_file_string(conf, &path, module_file)?;
     out.loaded_from = LoadedFrom::Implied;
     Ok(out)
 }
@@ -187,4 +362,20 @@ pub fn do_something_with_module(_stream: TokenStream) -> TokenStream {
         }
     });
     out
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_remove_recursive_macro() {
+        let code = r#"
+        #[hira::hira] mod _typehints {}
+        fn hello() {}
+        "#;
+        let mut conf = HiraConfig::default();
+        let res = load_module_from_file_string(&mut conf, "a", code.to_string()).unwrap();
+        assert_eq!(res.contents, "fn hello () { }\n");
+    }
 }
--- hira_lib/src/parsing.rs
+++ hira_lib/src/parsing.rs
@@ -8,6 +8,23 @@ use proc_macro2::{
     TokenStream,
     TokenTree,
 };
+use quote::ToTokens;
+use syn::{
+    File,
+    Item,
+    ItemFn,
+    ItemConst,
+    ItemType,
+    ItemEnum,
+    ItemMod,
+    ItemStatic,
+    ItemStruct,
+    ItemTrait,
+    ItemUnion,
+    Expr
+};
+
+use crate::module_loading::HiraModule;
 
 pub fn default_stream() -> TokenStream {
     compiler_error("Failed to get hira config")
@@ -21,6 +38,135 @@ pub fn compiler_error(msg: &str) -> TokenStream {
             panic!("Failed to parse compiler_error formatting\n{:?}", e);
         }
     };
+    out
+}
+
+/// in a few places in hira we let the module writer specify some array of values
+/// which we parse out the strings. This function is generic over that iteration
+/// and calls the callback with anytime we find a string
+pub fn iterate_expr_for_strings(
+    expr: &Expr,
+    mut cb: impl FnMut(String)
+) {
+    let arr = match expr {
+        syn::Expr::Array(arr) => arr,
+        syn::Expr::Reference(r) => {
+            if let syn::Expr::Array(arr) = &*r.expr {
+                arr
+            } else {
+                return;
+            }
+        }
+        _ => {
+            return;
+        }
+    };
+    for item in arr.elems.iter() {
+        if let syn::Expr::Lit(l) = item {
+            if let syn::Lit::Str(s) = &l.lit {
+                let mut s = s.token().to_string();
+                remove_surrounding_quotes(&mut s);
+                cb(s);
+            }
+        }
+    }
+}
+
+/// iterates over a file, calls provided callbacks which can modify the
+/// hira module definition for the relevant fields.
+/// each callback returns whether or not the item should be added to the exported string.
+/// if any callback returns false, that particular item is ignored.
+/// finally, return the output string which is the filtered
+/// version of the file.
+pub fn iterate_file(
+    module: &mut HiraModule,
+    file: File,
+    fn_callbacks: &[fn(&mut HiraModule, &ItemFn) -> bool],
+    const_callbacks: &[fn(&mut HiraModule, &ItemConst) -> bool],
+    type_callbacks: &[fn(&mut HiraModule, &ItemType) -> bool],
+    enum_callbacks: &[fn(&mut HiraModule, &ItemEnum) -> bool],
+    mod_callbacks: &[fn(&mut HiraModule, &ItemMod) -> bool],
+    static_callbacks: &[fn(&mut HiraModule, &ItemStatic) -> bool],
+    struct_callbacks: &[fn(&mut HiraModule, &ItemStruct) -> bool],
+    trait_callbacks: &[fn(&mut HiraModule, &ItemTrait) -> bool],
+    union_callbacks: &[fn(&mut HiraModule, &ItemUnion) -> bool],
+) -> String {
+    let mut out = "".to_string();
+    for item in file.items.iter() {
+        let mut is_filtered = false;
+        match item {
+            Item::Fn(x) => {
+                for cb in fn_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            Item::Const(x) => {
+                for cb in const_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            Item::Enum(x) => {
+                for cb in enum_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            Item::Mod(x) => {
+                for cb in mod_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            Item::Static(x) => {
+                for cb in static_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            Item::Struct(x) => {
+                for cb in struct_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            Item::Trait(x) => {
+                for cb in trait_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            Item::Type(x) => {
+                for cb in type_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            Item::Union(x) => {
+                for cb in union_callbacks {
+                    if !cb(module, x) {
+                        is_filtered = true;
+                    }
+                }
+            }
+            // ignore all other item types
+            _ => {}
+        }
+        if !is_filtered {
+            let item_str = item.to_token_stream().to_string();
+            out.push_str(&item_str);
+            out.push('\n');
+        }
+    }
     out
 }
 
