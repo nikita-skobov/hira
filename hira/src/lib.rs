261a334bcfada77520a180788b197a76885f22fa
nikita-skobov <nikita@skobov.io>
2023-05-13 11:16:43 -0600

    hira proc-macro back to just 'hira'

--- hira/src/lib.rs
+++ hira/src/lib.rs
@@ -1,5 +1,5 @@
 #[proc_macro_attribute]
-pub fn hira_mod2(attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn hira(attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let item = proc_macro2::TokenStream::from(item);
     let attr = proc_macro2::TokenStream::from(attr);
     hira_lib::module_loading::hira_mod2(item, attr).into()
