45bcc83cdbbfaf765a9ffdad2e17e59c675c75f7
nikita-skobov <nikita@skobov.io>
2023-05-28 17:19:59 -0600

    example of lambda function URL as origin for CloudFront dist

--- real_world_examples/src/lib.rs
+++ real_world_examples/src/lib.rs
@@ -1,5 +1,6 @@
 use hira::hira;
 use aws_lambda::h_aws_lambda;
+use aws_cloudfront_distribution::aws_cloudfront_distribution;
 
 #[hira]
 pub mod my_lambda {
@@ -30,9 +31,29 @@ pub mod my_lambda {
 #[hira]
 pub mod other_lambda_fn {
     use super::h_aws_lambda;
+
+    pub mod outputs {
+        pub use super::h_aws_lambda::outputs::LOGICAL_FUNCTION_URL_NAME;
+    }
+
     pub fn config(_lambdainput: &mut h_aws_lambda::Input) {}
 
     pub fn lambda_main(a: h_aws_lambda::FunctionUrlEvent) -> String {
         format!("Other lambda received::\n{}", a.body)
+    }
+}
+
+#[hira]
+pub mod making_my_distr {
+    extern crate cfn_resources;
+    use super::other_lambda_fn::outputs::*;
+    use super::aws_cloudfront_distribution;
+    use self::aws_cloudfront_distribution::CustomOriginConfigOriginProtocolPolicyEnum;
+
+    pub fn config(distrinput: &mut aws_cloudfront_distribution::Input) {
+        let func_url = cfn_resources::get_att(LOGICAL_FUNCTION_URL_NAME, "FunctionUrl");
+        let select_domain = cfn_resources::select_split(2, "/", func_url);
+        distrinput.default_origin_domain_name = cfn_resources::StrVal::Val(select_domain);
+        distrinput.default_origin_protocol_policy = CustomOriginConfigOriginProtocolPolicyEnum::Httpsonly;
     }
 }
