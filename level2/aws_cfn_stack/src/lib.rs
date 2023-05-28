884d7ee0945075ddce9be9ce06629b447be1ab9c
nikita-skobov <nikita@skobov.io>
2023-05-28 16:35:24 -0600

    fixes call to runtime main

--- level2/aws_cfn_stack/src/lib.rs
+++ level2/aws_cfn_stack/src/lib.rs
@@ -344,7 +344,7 @@ pub mod aws_cfn_stack {
         for code in input.run_before.iter() {
             runtimer.add_to_runtime_unique_beginning("deployer", code.to_string());
         }
-        runtimer.add_to_runtime_unique_end("deployer", "aws_cfn_stack::runtime_main(&runtime_data).await".to_string());
+        runtimer.add_to_runtime_unique_end("deployer", "::aws_cfn_stack::runtime_main(&runtime_data).await".to_string());
         runtimer.add_data_to_runtime("deployer", output);
     }
 }
