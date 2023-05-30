89e5c42c63c5d44f593d66a1b7e7983ffcdaf4ba
nikita-skobov <nikita@skobov.io>
2023-05-30 17:25:22 -0600

    adds dotenv lvl2 module

---
+++ level2/dotenv_reader/src/lib.rs
@@ -0,0 +1,18 @@
+use hira_lib::level0::L0Core;
+
+#[hira::hira]
+pub mod dotenv_reader {
+    use super::L0Core;
+
+    #[derive(Default)]
+    pub struct Input {
+        /// the path to your .env file.
+        /// it should be relative to the root of your crate, ie:
+        /// the same directory as your Cargo.toml file.
+        pub dotenv_path: &'static str,
+    }
+
+    pub fn config(myinp: &mut Input, l0core: &mut L0Core) {
+        l0core.set_dotenv_location(myinp.dotenv_path);
+    }
+}
