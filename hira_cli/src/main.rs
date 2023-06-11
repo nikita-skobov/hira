67a5778cdd374b6e3c6c032b8475b58b857f3ebd
nikita-skobov <nikita@skobov.io>
2023-06-11 14:47:52 -0600

    hira cli can build and run a specific runtime

--- hira_cli/src/main.rs
+++ hira_cli/src/main.rs
@@ -1,5 +1,5 @@
-use std::path::{Path, PathBuf};
-use hira_lib::{HiraConfig, parsing::{iter_hira_modules, get_ident_string}, module_loading::print_debug};
+use std::{path::{Path, PathBuf}, process::{Command, Stdio}};
+use hira_lib::{HiraConfig, parsing::{iter_hira_modules, get_ident_string}, module_loading::print_debug, level0::RuntimeMeta};
 use quote::ToTokens;
 
 
@@ -41,6 +41,7 @@ fn find_closest_cargo_toml(mut search_dir: PathBuf) -> Option<PathBuf> {
 }
 
 fn main() {
+    let compile_and_run_runtime = std::env::args().nth(1);
     let cargo_home = env!("CARGO_HOME");
     std::env::set_var("CARGO_HOME", cargo_home);
     let currdir = std::env::current_dir().expect("Failed to get current directory");
@@ -67,7 +68,7 @@ fn main() {
         eprintln!("{e}");
         std::process::exit(1);
     }
-    let _conf = match fill_hira_graph(&all_rust_files) {
+    let mut conf = match fill_hira_graph(&all_rust_files) {
         Ok(c) => c,
         Err(e) => {
             eprintln!("{e}");
@@ -75,7 +76,97 @@ fn main() {
         }
     };
 
-    // println!("{:#?}", conf);
+    if let Err(e) = build_runtimes(&mut conf, compile_and_run_runtime) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn build_runtimes(
+    conf: &mut HiraConfig,
+    compile_and_run_runtime: Option<String>
+) -> Result<(), String> {
+    let logfile = conf.logfile.clone();
+    // if a runtime was specified, compile only that runtime and then run it.
+    // otherwise, simply compile all runtimes, but dont run them.
+    if let Some(runtime_name) = compile_and_run_runtime {
+        // first compile all dependencies of this runtime
+        if let Some(dependencies) = conf.runtime_depends_on.get(&runtime_name) {
+            for dep_name in dependencies.iter() {
+                if let Some((_, runtime, _, _)) = conf.runtimes.get(dep_name) {
+                    build_runtime(
+                        &dep_name, &conf.wasm_directory,
+                        &conf.runtime_directory, &conf.crate_name,
+                        runtime, &logfile, Some(&runtime_name),
+                    )?;
+                } else {
+                    return Err(format!("Failed to find dependency {} on runtime {}", dep_name, runtime_name));
+                }
+            }
+        }
+        if let Some((_, runtime, _, _)) = conf.runtimes.get(&runtime_name) {
+            // now build and run the requested runtime.
+            build_runtime(
+                &runtime_name, &conf.wasm_directory,
+                &conf.runtime_directory, &conf.crate_name,
+                runtime, &logfile, None,
+            )?;
+            run_runtime(&conf.runtime_directory, &runtime_name)?;
+        } else {
+            return Err(format!("Runtime {} does not exist", runtime_name));
+        }
+    } else {
+        // otherwise just build all runtimes but dont run them.
+        for (name, (_, runtime, _, _)) in conf.runtimes.iter() {
+            build_runtime(
+                &name, &conf.wasm_directory,
+                &conf.runtime_directory, &conf.crate_name,
+                runtime, &logfile, None
+            )?;
+        }
+    }
+    Ok(())
+}
+
+fn run_runtime(
+    runtime_dir: &str,
+    name: &str,
+) -> Result<(), String> {
+    let cmd = format!("{runtime_dir}/{name}");
+    let out = Command::new(&cmd)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .stdin(Stdio::null())
+        .output().map_err(|e| format!("Failed to invoke {cmd}\n{:?}", e))?;
+    if !out.status.success() {
+        return Err(format!("Failed to run runtime {name}"));
+    }
+
+    Ok(())
+}
+
+fn build_runtime(
+    name: &str,
+    wasm_dir: &str,
+    runtime_dir: &str,
+    crate_name: &str,
+    runtime: &RuntimeMeta,
+    logfile: &str,
+    dependency_of: Option<&String>,
+) -> Result<(), String> {
+    let target_dir = format!("{}/target_{}", wasm_dir, name);
+    let hira_runtime_output_path = format!("{}/{}", runtime_dir, name);
+    if let Some(dep) = &dependency_of {
+        println!("Building runtime {name} (dependency of {dep})");
+    } else {
+        println!("Building runtime {name}");
+    }
+    let now = std::time::Instant::now();
+    HiraConfig::run_build_runtime_cmd(runtime, &name, &target_dir, crate_name, &hira_runtime_output_path)?;
+    let elapsed = now.elapsed().as_millis();
+    let contents = format!("Building {name}, dur={elapsed}ms\n");
+    print_debug(logfile, &contents);
+    Ok(())
 }
 
 fn compile_log(name: &str) -> String {
@@ -105,17 +196,6 @@ fn fill_hira_graph(files: &Vec<PathBuf>) -> Result<HiraConfig, String> {
             print_debug(&logfile, &contents);
             Ok(true)
         }).map_err(|e| format!("Failed to get hira modules from {:?}\n{:?}", f, e))?;
-    }
-
-    for (name, (_, runtime, _, _)) in conf.runtimes.iter() {
-        let target_dir = format!("{}/target_{}", conf.wasm_directory, name);
-        let hira_runtime_output_path = format!("{}/{}", conf.runtime_directory, name);
-        println!("Building runtime {name}");
-        let now = std::time::Instant::now();
-        HiraConfig::run_build_runtime_cmd(runtime, &name, &target_dir, &conf.crate_name, &hira_runtime_output_path)?;
-        let elapsed = now.elapsed().as_millis();
-        let contents = format!("Building {name}, dur={elapsed}ms\n");
-        print_debug(&logfile, &contents);
     }
     Ok(conf)
 }
